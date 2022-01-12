7aa27d6f4955d1fc31f425527e0dd71fcfc9aaa0
Ertuğrul Gürkan <ertugrulgurkan16@gmail.com>
2022-01-12 18:54:13 +0300

    usercontroller and service were implemented.

---
+++ src/main/java/com/ertugrul/spring/controller/UserController.java
@@ -0,0 +1,42 @@
+package com.ertugrul.spring.controller;
+
+
+import com.ertugrul.spring.dto.UserDto;
+import com.ertugrul.spring.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/users")
+@CrossOrigin
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getById(@PathVariable Long id){
+
+        UserDto userDto = userService.findById(id);
+
+        return ResponseEntity.ok(userDto);
+    }
+    
+    @GetMapping("/username/{username}")
+    public ResponseEntity<Object> getByUsername(@PathVariable String username){
+
+        UserDto userDto = userService.findByUsername(username);
+
+        return ResponseEntity.ok(userDto);
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> create(@RequestBody UserDto userDto){
+        return ResponseEntity.ok(userService.save(userDto));
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id){
+        userService.delete(id);
+    }
+}
--- src/main/java/com/ertugrul/spring/service/UserService.java
+++ src/main/java/com/ertugrul/spring/service/UserService.java
@@ -1,11 +1,76 @@
 package com.ertugrul.spring.service;
 
+import com.ertugrul.spring.converter.UserMapper;
+import com.ertugrul.spring.dto.UserDto;
+import com.ertugrul.spring.entity.User;
+import com.ertugrul.spring.exception.UserNotFoundException;
 import com.ertugrul.spring.service.entityservice.UserEntityService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserEntityService userEntityService;
+    public List<UserDto> findAll() {
+
+        List<User> userList = userEntityService.findAll();
+
+        List<UserDto> userDtoList = UserMapper.INSTANCE.convertAllUserDtoToUser(userList);
+
+        return userDtoList;
+    }
+
+    public UserDto findById(Long id) {
+
+        User user = findUserById(id);
+
+        UserDto userDto = UserMapper.INSTANCE.convertUserDtoToUser(user);
+
+        return userDto;
+    }
+
+    public UserDto findByUsername(String username) {
+
+        User user = userEntityService.findByUsername(username);
+
+        if (user == null){
+            throw new RuntimeException("User not found!");
+        }
+
+        UserDto userDto = UserMapper.INSTANCE.convertUserDtoToUser(user);
+
+        return userDto;
+    }
+
+    public UserDto save(UserDto userDto) {
+
+        User user = UserMapper.INSTANCE.convertUserDtoToUser(userDto);
+
+        user = userEntityService.save(user);
+
+        return UserMapper.INSTANCE.convertUserDtoToUser(user);
+    }
+
+    public void delete(Long id) {
+
+        User user = findUserById(id);
+
+        userEntityService.delete(user);
+    }
+
+    private User findUserById(Long id) {
+        Optional<User> optionalUser = userEntityService.findById(id);
+
+        User user;
+        if (optionalUser.isPresent()){
+            user = optionalUser.get();
+        } else {
+            throw new UserNotFoundException("User not found!");
+        }
+        return user;
+    }
 }
